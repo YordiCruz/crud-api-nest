a44afa5f990966c03beeaf3e75fe96ff8c9fd208
Yordi <yordi.cruz@gmail.com>
2025-06-02 19:01:20 -0400

    practicando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //agregamos swagger segun la documentacion
+   const config = new DocumentBuilder()
+    .setTitle('Crud Productos')
+    .setDescription('La Api de Productos - todo el crud de productos')
+    .setVersion('1.0')
+    .addTag('producto')
+    .build();
+
+     const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   //agregamos la validación como indica la documentación
   app.useGlobalPipes(new ValidationPipe({
     //disableErrorMessages: true
@@ -11,6 +23,7 @@ async function bootstrap() {
     whitelist: true,
     //con whitelist y el forbidNonWhitelisted indican que datos no permitimos 
     forbidNonWhitelisted: true
+    //swagger es una interfaz q se habilitara y con eso se puede probar los servicios sin necesidad de usar postman imsomnia u otro
   }));
 
   await app.listen(process.env.PORT ?? 3000);
--- src/producto/dto/create-producto.dto.ts
+++ src/producto/dto/create-producto.dto.ts
@@ -1,15 +1,24 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsBoolean, IsNumber, IsString } from "class-validator";
 
 //se parece al archivo Producto.entity pero se diferenciara por
 export class CreateProductoDto {
+
+
+    @ApiProperty()
     @IsString()
     nombre: string;
     
+    //con lo siguiente la propiedad aparecera en swagger, cosa que solo se ponga el valor 
+    @ApiProperty()
+
     @IsNumber()
     precio: number;
-    
+
+    @ApiProperty()
+    @ApiPropertyOptional()
     @IsBoolean()
     estado: boolean;
 
-    
+
 }   
